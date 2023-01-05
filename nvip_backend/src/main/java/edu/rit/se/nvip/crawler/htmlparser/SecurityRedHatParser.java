c6b7d57ede030de75340aba31218fed011a09fff
aep7128 <aep7128@g.rit.edu>
2023-01-05 12:04:36 -0500

    finished redhat security parser

--- nvip_backend/src/main/java/edu/rit/se/nvip/crawler/htmlparser/SecurityRedHatParser.java
+++ nvip_backend/src/main/java/edu/rit/se/nvip/crawler/htmlparser/SecurityRedHatParser.java
@@ -26,13 +26,10 @@ package edu.rit.se.nvip.crawler.htmlparser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
