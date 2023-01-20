026615d776ede2aa71f7c1256639aaf5c76ce363
Corey Urbanke <cju8676@rit.edu>
2023-01-20 16:16:57 -0500

    finished porting over remainder of html
    - structure and rest of static html migrated, commented out some vars for now
    - most of UI shell under Angular resembling old AngularJS with some kinks here and there to iron out
    - routing to these pages alongside static html

---
+++ nvip_ui/angular-ui/src/app/components/create-account/create-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account',
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.css']
+})
+export class CreateAccountComponent {
+
+}
---
+++ nvip_ui/angular-ui/src/app/pages/daily/daily.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily',
+  templateUrl: './daily.component.html',
+  styleUrls: ['./daily.component.css']
+})
+export class DailyComponent {
+
+}
