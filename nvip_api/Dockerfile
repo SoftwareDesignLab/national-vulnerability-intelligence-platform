42fc1c165be644eae810bf3a38d9698adcdd88ac
Chris Enoch <ctevse@rit.edu>
2023-02-16 22:29:52 +0000

    Refactored DBConnect to use environment variables for DB connections

--- nvip_api/Dockerfile
+++ nvip_api/Dockerfile
@@ -1,18 +1,21 @@
-FROM maven:3.8-jdk-11 as builder
+### Build Stage
+FROM maven:3.8-jdk-11-slim AS builder
+
+WORKDIR /home/app
 
 ADD pom.xml .
-ADD .project .
-ADD .classpath .
-ADD src/ src/
+RUN mvn dependency:go-offline
+ADD docker.context.xml WebContent/META-INF/context.xml
+ADD src/main src/main
 ADD WebContent WebContent/
 
-ADD docker.context.xml WebContent/META-INF/context.xml
+RUN mvn package -Dmaven.test.skip=true
 
-RUN mvn clean package -Dskiptests
-
+### Run Stage
 FROM tomcat:9.0-alpine as deploy
 
-COPY --from=builder /target/nvip_ui-1.0.war /usr/local/tomcat/webapps
+RUN rm -r /usr/local/tomcat/webapps/ROOT
+COPY --from=builder /home/app/target/nvip_ui-1.0.war /usr/local/tomcat/webapps/ROOT.war
 
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
--- nvip_api/src/main/java/data/DBConnect.java
+++ nvip_api/src/main/java/data/DBConnect.java
@@ -23,11 +23,11 @@
  */
 package data;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.apache.logging.log4j.LogManager;
@@ -36,22 +36,87 @@ import org.apache.logging.log4j.Logger;
 public class DBConnect {
 	private static Logger logger = LogManager.getLogger(DBConnect.class);
 
-	public static Connection getConnection() {
+	public static void main(String[] args) throws SQLException {
+		try(Connection conn = getConnection();
+			Statement stmt = conn.createStatement()
+		) {
+			ResultSet rs = stmt.executeQuery("SHOW TABLES;");
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			logger.info("Number of Columns: " + columnsNumber);
+			while (rs.next()) {
+				StringBuilder rowInfo = new StringBuilder();
+				for (int i = 1; i <= columnsNumber; i++) {
+					if (i > 1) rowInfo.append(",  ");
+					String columnValue = rs.getString(i);
+					rowInfo.append(columnValue).append(" ").append(rsmd.getColumnName(i));
+				}
+				logger.info(rowInfo);
+			}
+			rs.close();
+		}
+	}
 
+	public static Connection getConnection() throws SQLException {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Context ctx = new InitialContext();
-			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/nvip_db_mysql");
-			Connection conn = ds.getConnection();
-			return conn;
-		} catch (SQLException e) {
-			logger.error("Username or Password for NVIP database is incorrect, please check context.xml to correct --> "
-					+ e.toString());
-		} catch (Exception e) {
-			logger.error(e.toString());
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
 		}
 
-		return null;
+		Connection conn = getConnectionFromEnvironment();
+
+		if(conn == null) {
+			conn = getConnectionFromContext();
+		}
+
+		if(conn == null) {
+			throw new NullPointerException("Unable to create connection from Environment: " + System.getenv("JDBC_CONNECTION_STRING"));
+		}
+
+		return conn;
+	}
+
+	private static Connection getConnectionFromContext() {
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/nvip_db_mysql");
+			ctx.close();
+			conn = ds.getConnection();
+		} catch (NamingException ex) {
+			logger.error("Unable to create DataSource from Context");
+			logger.error(ex.getMessage());
+		} catch (SQLException ex) {
+			logger.error("Username or Password for NVIP database is incorrect, please check context.xml to correct --> " + ex.toString());
+			logger.error(ex.getMessage());
+		}
+		return conn;
+	}
+
+	/**
+	 * Retrieves and builds a connection from an environment or system property named JDBC_CONNECTION_STRING
+	 *
+	 * @return
+	 */
+	private static Connection getConnectionFromEnvironment() throws NullPointerException {
+		Connection conn;
+
+		String jdbcConnString = System.getenv("JDBC_CONNECTION_STRING");
+
+		if (jdbcConnString == null) {
+			// checks to see if the envvar is set as a system property
+			jdbcConnString = System.getProperty("JDBC_CONNECTION_STRING");
+		}
+
+		try {
+			conn = DriverManager.getConnection(jdbcConnString);
+		} catch (SQLException ex) {
+			logger.error("Username or Password for NVIP database is incorrect, please check context.xml to correct --> " + ex.toString());
+			logger.error(ex.getMessage());
+			throw new NullPointerException(ex.getMessage());
+		}
+		return conn;
 	}
 
 	public static String getDatabaseType() {
