2ec594fc94dd72992765fd4d07855c9abd7ed5ca
Corey Urbanke <cju8676@rit.edu>
2023-02-15 23:31:39 -0500

    Added KbCert single test and serial ID in CpeGroup
    + serial ID to avoid InvalidClassException: local class incompatible when trying to load CPE dictionary from CPEmap.ser file

--- nvip_backend/src/test/java/edu/rit/se/nvip/crawler/htmlparser/KbCertCveParserTest.java
+++ nvip_backend/src/test/java/edu/rit/se/nvip/crawler/htmlparser/KbCertCveParserTest.java
@@ -24,4 +24,15 @@ public class KbCertCveParserTest extends AbstractParserTest {
         assertTrue(vuln.getDescription().contains("susceptible to a side-channel attack"));
         assertFalse(vuln.getDescription().contains("HTTP Basic Authentication mode"));
     }
+
+    @Test
+    public void testKbCertSingleCVE() {
+        String html = safeReadHtml("src/test/resources/test-kb-cert-single.html");
+        List<CompositeVulnerability> list = new KbCertCveParser("kb.cert").parseWebPage("kb.cert", html);
+        assertEquals(1, list.size());
+        CompositeVulnerability vuln = getVulnerability(list, "CVE-2021-33164");
+        assertNotNull(vuln);
+        assertTrue(vuln.getDescription().contains("access and validation of the SMRAM"));
+        assertEquals("2022/11/08 00:00:00", vuln.getPublishDate());
+    }
 }
