cdb6fb4d5b3f33b6d8c4631934992f4524a23fad
jqm4954 <jqm4954@rit.edu>
2023-02-21 15:04:02 -0500

    Expanded full coverage for EntropyClassifier

--- nvip_backend/src/test/java/edu/rit/se/nvip/characterizer/classifier/EntropyBasedCveClassifierTest.java
+++ nvip_backend/src/test/java/edu/rit/se/nvip/characterizer/classifier/EntropyBasedCveClassifierTest.java
@@ -1,16 +1,17 @@
 package edu.rit.se.nvip.characterizer.classifier;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import edu.rit.se.nvip.characterizer.CveCharacterizer;
+import edu.rit.se.nvip.characterizer.CvePreProcessor;
 import edu.rit.se.nvip.utils.MyProperties;
 import edu.rit.se.nvip.utils.PropertyLoader;
 import org.junit.Test;
-import weka.core.*;
+import weka.core.Instance;
+import weka.core.SparseInstance;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class EntropyBasedCveClassifierTest {
     @Test
@@ -55,7 +56,7 @@ public class EntropyBasedCveClassifierTest {
     }
 
     @Test
-    public void testPredictCorrectNumAttributes() {
+    public void testPredict() {
         MyProperties propertiesNvip = new MyProperties();
         propertiesNvip = new PropertyLoader().loadConfigFile(propertiesNvip);
         String[] trainingDataInfo = propertiesNvip.getCveCharacterizationTrainingDataInfo();
@@ -67,15 +68,19 @@ public class EntropyBasedCveClassifierTest {
 
         // pre-process training data and store it
         String preProcessedTrainingDataFile = trainingDataFileName.concat("-processed.csv");
-
         EntropyBasedCveClassifier entropyBasedCveClassifier = new EntropyBasedCveClassifier(preProcessedTrainingDataFile);
         String cveDesc = "7.2 HIGH9.0 HIGHCVE-2020-11544 Ã¢â‚¬â€� An issue was discovered in Project Worlds Official Car Rental System 1. It allows the admin user to run commands on the server with their account because the upload section on the file-manager page contains an arbitrary file upload vulnerability via... read CVE-2020-11544 Published: April 06, 2020; 12:15:13 PM -04:00 CVE-2020-11544read CVE-2020-11544V3.1:7.2 HIGH6.5 MEDIUM";
 
-        CveCharacterizer cveCharacterizer = new CveCharacterizer(trainingDataPath, trainingDataFiles, "IT", "NB", true);
-        Map<String,ArrayList<String[]>> prediction = cveCharacterizer.characterizeCveForVDO(cveDesc, true);
-        //ArrayList newList = entropyBasedCveClassifier.predict(newInstance, false);
-        //assertEquals(0, newList.size());
-        assertTrue(prediction.size() > 0);
-        cveCharacterizer = null;
+        CvePreProcessor cvePreProcessor = new CvePreProcessor(true);
+        String cveDescProcessed = cvePreProcessor.preProcessLine(cveDesc);
+
+        Map<String, ArrayList<String[]>> prediction = new HashMap<String, ArrayList<String[]>>();
+
+        entropyBasedCveClassifier.trainMLModel();
+        ArrayList<String[]> predictionFromClassifier = entropyBasedCveClassifier.predict(cveDescProcessed, true);
+        String vdoNounGroup = entropyBasedCveClassifier.getCveClassifierName();
+        prediction.put(vdoNounGroup, predictionFromClassifier);
+
+        assertEquals(1, prediction.size());
     }
 }
