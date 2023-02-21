923b9af9e08784004f80bbed3fa1c8f763c4bf73
James <jqm4954@rit.edu>
2023-02-20 20:53:35 -0500

    Expanded full coverage for OrdinaryCveClassifier and CveCharacterizer

--- nvip_backend/src/test/java/edu/rit/se/nvip/characterizer/classifier/EntropyBasedCveClassifierTest.java
+++ nvip_backend/src/test/java/edu/rit/se/nvip/characterizer/classifier/EntropyBasedCveClassifierTest.java
@@ -1,13 +1,16 @@
 package edu.rit.se.nvip.characterizer.classifier;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import edu.rit.se.nvip.characterizer.CveCharacterizer;
 import edu.rit.se.nvip.utils.MyProperties;
 import edu.rit.se.nvip.utils.PropertyLoader;
 import org.junit.Test;
 import weka.core.*;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class EntropyBasedCveClassifierTest {
     @Test
@@ -49,5 +52,30 @@ public class EntropyBasedCveClassifierTest {
         Instance newInstance = new SparseInstance(293);
         ArrayList newList = entropyBasedCveClassifier.predict(newInstance, false);
         assertEquals(0, newList.size());
+    }
+
+    @Test
+    public void testPredictCorrectNumAttributes() {
+        MyProperties propertiesNvip = new MyProperties();
+        propertiesNvip = new PropertyLoader().loadConfigFile(propertiesNvip);
+        String[] trainingDataInfo = propertiesNvip.getCveCharacterizationTrainingDataInfo();
+        String trainingDataPath = trainingDataInfo[0];
+        String trainingDataFiles = trainingDataInfo[1];
+        String[] trainingDataFileArr = trainingDataFiles.split(",");
+        String trainingDataFileName = trainingDataFileArr[0];
+        trainingDataFileName = trainingDataPath + trainingDataFileName;
+
+        // pre-process training data and store it
+        String preProcessedTrainingDataFile = trainingDataFileName.concat("-processed.csv");
+
+        EntropyBasedCveClassifier entropyBasedCveClassifier = new EntropyBasedCveClassifier(preProcessedTrainingDataFile);
+        String cveDesc = "7.2 HIGH9.0 HIGHCVE-2020-11544 Ã¢â‚¬â€� An issue was discovered in Project Worlds Official Car Rental System 1. It allows the admin user to run commands on the server with their account because the upload section on the file-manager page contains an arbitrary file upload vulnerability via... read CVE-2020-11544 Published: April 06, 2020; 12:15:13 PM -04:00 CVE-2020-11544read CVE-2020-11544V3.1:7.2 HIGH6.5 MEDIUM";
+
+        CveCharacterizer cveCharacterizer = new CveCharacterizer(trainingDataPath, trainingDataFiles, "IT", "NB", true);
+        Map<String,ArrayList<String[]>> prediction = cveCharacterizer.characterizeCveForVDO(cveDesc, true);
+        //ArrayList newList = entropyBasedCveClassifier.predict(newInstance, false);
+        //assertEquals(0, newList.size());
+        assertTrue(prediction.size() > 0);
+        cveCharacterizer = null;
     }
 }
