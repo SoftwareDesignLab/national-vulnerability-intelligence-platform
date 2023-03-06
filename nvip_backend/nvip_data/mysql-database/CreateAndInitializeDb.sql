be1ec765dc9371c9f8969f9fcdd2724c8a533a67
Chris Enoch <ctevse@rit.edu>
2023-03-06 21:51:15 +0000

    Fixed issue where StoredProcedure would cause an exception
    - MySQL's group_concat should use DISTINCT to prevent duplicates, and prevents exceeding the max_length var
      - Without this, rows were being dropped from the query and causing a Java SQLException

--- nvip_backend/nvip_data/mysql-database/CreateAndInitializeDb.sql
+++ nvip_backend/nvip_data/mysql-database/CreateAndInitializeDb.sql
@@ -744,40 +744,87 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `prepareDailyVulnerabilities`(IN start_date TIMESTAMP, IN end_date TIMESTAMP, OUT cveCount int)
+CREATE DEFINER = root@localhost PROCEDURE prepareDailyVulnerabilities(IN start_date timestamp, IN end_date timestamp, OUT cveCount int)
 BEGIN
 SET session group_concat_max_len=100000;
-DELETE FROM vulnerabilityaggregate;
-INSERT INTO vulnerabilityaggregate(SELECT v.vuln_id, v.cve_id, v.description, v.platform, v.published_date, v.last_modified_date, v.fixed_date, v.exists_at_nvd, v.exists_at_mitre, vc.vdo_labels, 
-	vc.vdo_label_confidences, vc.vdo_noun_groups, vsu.urls, cs.base_severities, cs.severity_confidences, cs.impact_scores, cs.impact_confidences, vap.product_id, vap.cpe, 
-    vap.domain, vap.version, expl.publish_date, expl.publisher_url, vu.run_date_time 
-	FROM (SELECT vu.vuln_id, MAX(drh.run_id) AS "run_id", MAX(drh.run_date_time) AS "run_date_time" FROM dailyrunhistory drh
-	INNER JOIN vulnerabilityupdate vu ON vu.run_id = drh.run_id 
-	WHERE drh.run_date_time BETWEEN start_date AND end_date GROUP BY vu.vuln_id) vu
-	INNER JOIN vulnerability v 
-	ON v.vuln_id = vu.vuln_id
-	LEFT JOIN (SELECT vc.cve_id, group_concat(vl.vdo_label_name SEPARATOR ";") AS vdo_labels, group_concat(vc.vdo_confidence SEPARATOR ";") AS vdo_label_confidences, 
-group_concat(ifnull(vn.vdo_noun_group_name, "None") SEPARATOR ";") AS vdo_noun_groups FROM vdocharacteristic vc 
-	INNER JOIN vdonoungroup vn ON vn.vdo_noun_group_id = vc.vdo_noun_group_id
-	INNER JOIN vdolabel vl ON vl.vdo_label_id = vc.vdo_label_id GROUP BY vc.cve_id) vc 
-	ON vc.cve_id = v.cve_id 
-	LEFT JOIN (SELECT cve_id, group_concat(url SEPARATOR ";") AS urls FROM vulnsourceurl GROUP BY cve_id) vsu 
-	ON vsu.cve_id = v.cve_id 
-	LEFT JOIN (SELECT csc.cve_id, group_concat(cse.cvss_severity_class SEPARATOR ";") AS base_severities, group_concat(csc.severity_confidence SEPARATOR ";") 
-	AS severity_confidences, group_concat(csc.impact_score SEPARATOR ";") AS impact_scores, group_concat(csc.impact_confidence SEPARATOR ";")  AS impact_confidences 
-	FROM cvssscore csc INNER JOIN cvssseverity cse ON cse.cvss_severity_id = csc.cvss_severity_id GROUP BY csc.cve_id) cs 
-	ON cs.cve_id = v.cve_id 
-    LEFT JOIN (SELECT cve_id, group_concat(ar.product_id SEPARATOR ";") AS product_id, group_concat(cpe SEPARATOR ";") AS cpe, group_concat(domain SEPARATOR ";") AS domain, group_concat(version SEPARATOR ";") AS version FROM affectedrelease ar
-	INNER JOIN product p ON p.product_id = ar.product_id GROUP BY cve_id) vap
-    ON vap.cve_id = v.cve_id
-    LEFT JOIN exploit as expl on expl.vuln_id = v.vuln_id
-    WHERE v.status_id <> 2 and v.description is not null ORDER BY v.vuln_id desc);
-
--- Remove certain CVEs
-DELETE FROM nvip.vulnerabilityaggregate WHERE description like '%** RESERVED ** This candidate%' or description like '%** REJECT ** DO NOT USE%';
-DELETE FROM nvip.vulnerabilityaggregate WHERE length(description) < 50; 
+TRUNCATE TABLE vulnerabilityaggregate;
+INSERT INTO vulnerabilityaggregate(SELECT v.vuln_id,
+                                          v.cve_id,
+                                          v.description,
+                                          v.platform,
+                                          v.published_date,
+                                          v.last_modified_date,
+                                          v.fixed_date,
+                                          v.exists_at_nvd,
+                                          v.exists_at_mitre,
+                                          vc.vdo_labels,
+                                          vc.vdo_label_confidences,
+                                          vc.vdo_noun_groups,
+                                          vsu.urls,
+                                          cs.base_severities,
+                                          cs.severity_confidences,
+                                          cs.impact_scores,
+                                          cs.impact_confidences,
+                                          vap.product_id,
+                                          vap.cpe,
+                                          vap.domain,
+                                          vap.version,
+                                          expl.publish_date,
+                                          expl.publisher_url,
+                                          vu.run_date_time
+                                   FROM (SELECT vu.vuln_id,
+                                                MAX(drh.run_id)        AS "run_id",
+                                                MAX(drh.run_date_time) AS "run_date_time"
+                                         FROM dailyrunhistory drh
+                                                  INNER JOIN vulnerabilityupdate vu ON vu.run_id = drh.run_id
+                                         WHERE drh.run_date_time BETWEEN start_date AND end_date
+                                         GROUP BY vu.vuln_id) vu
+                                            INNER JOIN vulnerability v
+                                                       ON v.vuln_id = vu.vuln_id
+                                            LEFT JOIN (SELECT vc.cve_id,
+                                                              group_concat(DISTINCT vl.vdo_label_name SEPARATOR ';') AS vdo_labels,
+                                                              group_concat(DISTINCT vc.vdo_confidence SEPARATOR ';') AS vdo_label_confidences,
+                                                              group_concat(DISTINCT
+                                                                               ifnull(vn.vdo_noun_group_name, 'None')
+                                                                               SEPARATOR
+                                                                               ';')                                      AS vdo_noun_groups
+                                                       FROM vdocharacteristic vc
+                                                                INNER JOIN vdonoungroup vn ON vn.vdo_noun_group_id = vc.vdo_noun_group_id
+                                                                INNER JOIN vdolabel vl ON vl.vdo_label_id = vc.vdo_label_id
+                                                       GROUP BY vc.cve_id) vc
+                                                      ON vc.cve_id = v.cve_id
+                                            LEFT JOIN (SELECT cve_id,
+                                                              group_concat(DISTINCT url SEPARATOR ';') AS urls
+                                                       FROM vulnsourceurl
+                                                       GROUP BY cve_id) vsu
+                                                      ON vsu.cve_id = v.cve_id
+                                            LEFT JOIN (SELECT csc.cve_id,
+                                                              group_concat(DISTINCT cse.cvss_severity_class SEPARATOR ';') AS base_severities,
+                                                              group_concat(DISTINCT csc.severity_confidence SEPARATOR ';') AS severity_confidences,
+                                                              group_concat(DISTINCT csc.impact_score SEPARATOR ';')        AS impact_scores,
+                                                              group_concat(DISTINCT csc.impact_confidence SEPARATOR ';')   AS impact_confidences
+                                                       FROM cvssscore csc
+                                                                INNER JOIN cvssseverity cse ON cse.cvss_severity_id = csc.cvss_severity_id
+                                                       GROUP BY csc.cve_id) cs
+                                                      ON cs.cve_id = v.cve_id
+                                            LEFT JOIN (SELECT cve_id,
+                                                              group_concat(DISTINCT ar.product_id SEPARATOR ';') AS product_id,
+                                                              group_concat(DISTINCT cpe SEPARATOR ';')           AS cpe,
+                                                              group_concat(DISTINCT domain SEPARATOR ';')        AS domain,
+                                                                  group_concat(DISTINCT version SEPARATOR ';')       AS version
+                                                       FROM affectedrelease ar
+                                                           INNER JOIN product p ON p.product_id = ar.product_id
+                                                       GROUP BY cve_id) vap
+                                                      ON vap.cve_id = v.cve_id
+                                            LEFT JOIN exploit as expl on expl.vuln_id = v.vuln_id
+                                   WHERE v.status_id <> 2
+                                     and v.description is not null
+                                     and v.description not like '%** RESERVED ** This candidate%'
+                                     and v.description not like '%** REJECT ** DO NOT USE%'
+                                     and length(v.description) >= 50
+                                   ORDER BY v.vuln_id desc);
 SELECT count(*) INTO cveCount FROM vulnerabilityaggregate;
-END ;;
+END;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
