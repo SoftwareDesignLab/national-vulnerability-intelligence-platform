bf0e3fe46225637e5023879ce5dd5e113f7e5191
Andrew Pickard <59809184+aep7128@users.noreply.github.com>
2023-04-13 17:09:18 -0400

    Added https for prod

--- angular-ui/src/environments/environment.staging.ts
+++ angular-ui/src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: "http://nvip-api-dev.eba-cvdj355w.us-east-1.elasticbeanstalk.com/"
+    production: true,
+    apiUrl: "http://nvip-api-dev.eba-cvdj355w.us-east-1.elasticbeanstalk.com/"
 };
--- angular-ui/src/environments/environment.ts
+++ angular-ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "http://api.cve.live"
+    apiUrl: "https://api.cve.live"
 };
